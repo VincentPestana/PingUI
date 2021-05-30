37b95716dcee3643d1fd8f4b904ac588b38f83c5
Vincent Pestana <crashoveride001@gmail.com>
2021-05-30 08:33:59 +0200

    Pinging logic
    Pinging logic and output of results

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.NetworkInformation;
 using System.Text;
 
 namespace PingUI
@@ -12,6 +13,7 @@ namespace PingUI
 			// Get input from user AND convert to list
 			Console.WriteLine("Hit Escape to stop inputting urls");
 			var sbString = new StringBuilder();
+			var sbOutput = new StringBuilder();
 			ConsoleKeyInfo userKey;
 			do
 			{
@@ -21,13 +23,55 @@ namespace PingUI
 				// Check when user hits escape
 				userKey = Console.ReadKey();
 			} while (userKey.Key != ConsoleKey.Escape);
-			var test = 0;
-
+			
 			var userInputLines = sbString.ToString().Split(Environment.NewLine);
 
 			// TODO Loop through list running pings AND read the avg time
+			foreach (var line in userInputLines)
+			{
+				if (string.IsNullOrEmpty(line))
+					continue;
+
+				// TODO Figure out if url or IP address
+
+				// Check if its actually a url first
+				Uri uriResult;
+				bool result = Uri.TryCreate(line, UriKind.Absolute, out uriResult)
+					&& (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
+
+				// TODO Make request
+				Ping pingSender = new Ping();
+				PingOptions options = new PingOptions();
+
+				// Use the default Ttl value which is 128,
+				// but change the fragmentation behavior.
+				options.DontFragment = true;
+
+				// Create a buffer of 32 bytes of data to be transmitted.
+				string data = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+				byte[] buffer = Encoding.ASCII.GetBytes(data);
+				int timeout = 120;
+				PingReply reply = pingSender.Send(line, timeout, buffer, options);
+				if (reply.Status != IPStatus.Success)
+				{
+					// Ping didnt happen
+					sbOutput.Append($"{line} : Failed|");
+				}
+				else
+				{
+					// Ping happened
+					sbOutput.Append($"{line} : {reply.RoundtripTime}|");
+				}
+			}
 
 			// TODO Print nice table with urls/ips and times
+			foreach (var outLine in sbOutput.ToString().Split("|"))
+			{
+				Console.WriteLine(outLine);
+			}
+
+			Console.WriteLine("Enter to exit");
+			Console.ReadLine();
 		}
 	}
 }
